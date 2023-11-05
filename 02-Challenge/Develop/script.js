6b0fef05c33f65f6df5062236e6da220e698176d
Thomas Holtam <thomasjholtamIII@gmail.com>
2023-11-04 20:04:38 -0600

    Added function to the checkbox elements and expanded the slider within the JS, can be viewed within the console log.

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+const password = document.getElementById(`password`);
+
 // Drop Down Menu Code
 const generateButton = document.getElementById("generate");
 const optionsMenu = document.getElementById("options-menu");
@@ -9,29 +12,36 @@ generateButton.addEventListener("click", () => {
 
 
 //Slider code to sync the slider with the number range
-const characterAmountRange = document.getElementById (`characterAmountRange`)
-const characterAmountNumber = document.getElementById (`characterAmountNumber`)
-characterAmountNumber.addEventListener(`input`, syncCharacterAmount)
-characterAmountRange.addEventListener(`input`, syncCharacterAmount)
+const characterAmountRange = document.getElementById (`characterAmountRange`);
+const characterAmountNumber = document.getElementById (`characterAmountNumber`);
+characterAmountNumber.addEventListener(`input`, syncCharacterAmount);
+characterAmountRange.addEventListener(`input`, syncCharacterAmount);
 function syncCharacterAmount(e) {
   const value = e.target.value
       characterAmountNumber.value = value
       characterAmountRange.value = value
-  }
+  };
+
+//Checkbox variables
+const includeUppercaseElement = document.getElementById(`includeUppercase`);
+const includeNumbersElement = document.getElementById(`includeNumbers`);
+const includeSymbolsElement = document.getElementById(`includeSymbols`);
 
 
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+//Extension of Checkbox vairiables, adds an event listener to stop the generate button from refreshing the page,
+//This also checks the previous variables for checked value and will include their data if so
+//As well it takes the "password" variable and stores the generated text within it and display it on the page
+generateBtn.addEventListener("click", e => {
+  e.preventDefault();
+  const includeUppercase = includeUppercaseElement.checked;
+  const includeNumbers = includeNumbersElement.checked;
+  const includeSymbols = includeSymbolsElement.checked;
+  const characterAmount = characterAmountNumber.value;
+  var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  console.log(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+});
 
-}
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
